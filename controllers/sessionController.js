a8bc03e13425e8e09695abeaa6b25e31a39abf6a
Brittany Price <bprice0496@gmail.com>
2025-10-05 20:29:24 -0400

    Got parse validation errors working

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-// const parseVErr = require("../utils/parseValidationErrors.js");
+const parseVErr = require("../utils/parseValidationErrors.js");
 const flash = require("connect-flash");
 
 const registerShow = (req, res) => {
@@ -15,7 +15,7 @@ const registerDo = async(req, res, next) => {
         await User.create(req.body);
     } catch (e) {
         if (e.constructor.name === "ValidationError") {
-            // parseVErr(e, req);
+            parseVErr(e, req);
         } else if (e.name === "MongoServerError" && e.code === 1100) {
             req.flash("error", "That email address is already registered.");
         } else {
