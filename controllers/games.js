602e9430bdee07b7a0dcf2c7931dddc03b495fcb
Brittany Price <bprice0496@gmail.com>
2025-10-12 22:33:39 -0400

    Refactor part 1 to fix forms

--- controllers/games.js
+++ controllers/games.js
@@ -10,7 +10,6 @@ const { StatusCodes } = require('http-status-codes');
 const createGame = async (req, res) => {
    req.body.createdBy = req.user._id;
    const game = await Game.create(req.body);
-   res.status(StatusCodes.CREATED).json({ game });
    res.render("game", { game: null });
 };
 
@@ -21,7 +20,7 @@ const getAllGames = async (req, res) => {
 
 const getAGame = async (req, res) => {
   // get specific game by game & user Id
-   const { user:{ userId }, params:{ id: gameId } } = req;
+   const { id: gameId } = req;
    const game = await Game.findOne({ _id: gameId, createdBy: req.user._id });
    if (!game) {
      req.flash("error",`No game with id ${gameId}`);
@@ -30,7 +29,7 @@ const getAGame = async (req, res) => {
 };
 
 const updateGame = async (req, res) => {
-    const { body: {difficulty, mistakes, usedHints, status}, user: { userId }, params: { id: gameId }} = req;
+    const { body: {difficulty, mistakes, usedHints, status}, params: { id: gameId }} = req;
     // require all params to update
     if (difficulty === '' || mistakes === '' || usedHints === '' || status === '') {
        req.flash("error",'Difficulty, mistakes, usedHints, and status fields cannot be empty!');
@@ -38,13 +37,15 @@ const updateGame = async (req, res) => {
     const game = await Game.findOneAndUpdate({_id: gameId, createdBy: req.user._id}, req.body, {new: true, runValidators: true});
      if (!game) {
       req.flash("error",`No game with id ${gameId}`);
+     } else {
+       res.status(StatusCodes.OK).json({ game });
      }
-     res.status(StatusCodes.OK).json({ game });
+
      res.render("game", { game })
 };
 
 const deleteGame = async (req, res) => {
-    const { user: { userId }, params: { id: gameId }} = req;
+    const { id: gameId } = req;
     const game = await Game.findOneAndDelete({ _id: gameId, createdBy: req.user._id });
     if (!game) {
       req.flash("error",`No game with id ${gameId}`);
--- routes/games.js
+++ routes/games.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { gameShow, getAllGames, getAGame, createGame, updateGame, deleteGame } = require('../controllers/games');
-router.route('/').post(createGame).get(gameShow);
-router.route('/:id').get(getAGame).delete(deleteGame).patch(updateGame);
-router.route("/games").get(getAllGames);
+router.route('/').get(gameShow);
+router.route('/create').post(createGame);
+router.route('/edit/:id').patch(updateGame);
+router.route('/delete/:id').delete(deleteGame);
+router.route('/:id').get(getAGame);
 
 module.exports = router;
